d4d7a7ad1b18910846caabacb13b36bc76fc7b3c
Brettburnsj <brettburnsj@gmail.com>
2023-05-18 17:19:09 -0500

    minor tweaks - still broken

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,27 +1,25 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-function setEventListeners(formElement, config) {
+function setEventListeners(formElement, options) {
   const { inputSelector } = options;
-  //   const inputSelector = config.inputSelector;
-  const inputsElements = [...formElement.querySelectorAll(inputSelector)];
+  const inputElements = [...formElement.querySelectorAll(inputSelector)];
   console.log(inputElements);
 }
 
-function enableValidation(config) {
-  const formElements = [...document.querySelectorAll(config.formSelector)];
+function enableValidation(options) {
+  const formElements = [...document.querySelectorAll(options.formSelector)];
   formElements.forEach((formElement) => {
     formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
-
-    setEventListeners(formElement, config);
+    setEventListeners(formElement, options);
   });
 }
 
 const config = {
   formSelector: ".popup__form",
-  inputSelector: ".popup__input",
+  inputSelector: ".modal__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
